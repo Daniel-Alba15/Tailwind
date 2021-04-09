2b52061579a26f818a86ea3a9e8af0ee0c191e84
Daniel <danielorlandoa8@gmail.com>
2021-04-09 17:53:25 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    flex: {
+      '1': '1 1 300px'
+    },
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+      }
+    },
+    variants: {},
+    plugins: [],
+  }
+}

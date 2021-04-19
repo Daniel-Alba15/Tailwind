1ffc460b66faea03e67ae59ac9c162acbed74528
Daniel <danielorlandoa8@gmail.com>
2021-04-19 15:31:23 -0500

    minior css changes

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,9 @@ module.exports = {
         require('autoprefixer'),
         purgecss({
             content: ['./**/*.html']
-        })
+        }),
+        require('cssnano')({
+            preset: 'default',
+        }),
     ]
 }
